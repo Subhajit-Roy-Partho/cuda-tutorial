e72dc103917a6499d02be23d768050ef271cc213
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2023-02-06 17:41:25 -0700

    first commit

---
+++ Basics/hello.cu
@@ -0,0 +1,3 @@
+#include <iostream>
+
+__global__ void kernel<<<,>>>();
---
+++ C++ Cuda Basics/seqCuda.cu
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thrust/reduce.h>
+#include <thrust/sequence.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+using namespace std;
+
+int main(){
+    const int N=5e4;
+}
